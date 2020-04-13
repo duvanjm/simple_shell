1ffdc5f31828232e3e8a1affda5deca955bf1630
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-04-13 15:56:11 -0500

    Ctrl+D managment

--- functions.c
+++ functions.c
@@ -43,3 +43,24 @@ void _printString(char *str)
 			i++;
 		}
 }
+/**
+   * _strcmp - check the code for Holberton School students.
+    *	@s1: param
+    *	@s2: param
+     * Return: 0 if are equals, wathever if not.
+      */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			j = s1[i] - s2[i];
+			return (j);
+		}
+	}
+
+	return (0);
+}
